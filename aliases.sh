5adeea6c8cfc575ee11085b252f4de2e0e495970
Ezequiel Rosas <ezequiel.rosas@digitalonus.com>
2021-08-22 03:02:09 -0500

    update

--- aliases.sh
+++ aliases.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
-alias nsh='PATH="$NPATH" sh'
+
 alias ls='ls --color'
+alias ll='ls -lah'
+alias mv='mv -iv'
+
 alias f='less +IF -#30 -S -r -c -F -f'
 alias iec-i='numfmt --to=iec-i --from=iec-i'
 alias tf=terraform
 alias TF='TF_LOG=TRACE TF_LOG_PATH=.tflogt terraform'
-alias dockerpf='ssh -fNn -L 2424:/var/run/docker.sock'
-alias kw='kops_wrapper kops.out.env'
-alias jqdk="jq -er '.data | keys'"
-alias jqd="jq  -er '.data[\$key] | @base64d' --arg key"
+alias docker-port-forward='ssh -fNn -L 2424:/var/run/docker.sock'
 alias yml='yaml.py | nvim - -c "set ft=yaml"'
-alias _bgj_='jobs | awk -F"[][]" '\''{print "%"$2}'\'
-alias bkill='if test -n "$(_bgj_)"; then kill $(_bgj_); fi'
-cd_() { command pushd "${1:-$HOME}" &>/dev/null; };
-alias cd=cd_
+alias cd=pushd
 
 gittop() {
   local CDPATH
@@ -27,9 +24,5 @@ trim() {
   : "${_##${1:-}}"
   : "${_%%${2:-}}"
   printf '%s\n' "${_}"
-}
-
-reminder_date() {
-  stat "${HOME:?}/.config/reminders/${1:?}" | awk '$1 ~ /Modify:/ {print $2}'
 }
 
